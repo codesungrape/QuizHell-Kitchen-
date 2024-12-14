a106740a168cc4c91448955f6a7c93319b29579f
Shanti Rai <shantirai@MacBookAir.lan>
2024-12-14 10:28:48 +0000

    Changes the theme and CSS completely so now it's focused on Gordon Ramsay's Hell's Kitchen. I also removed the feedback container as it wasnt necessary

--- main.js
+++ main.js
@@ -1,59 +1,68 @@
-const renderAPI = document.getElementById("APIrender");
+/*
+ * Images of gordon responses that you can map through
+ * works out the IQ value at the end and tells user if they are genius or not
+ *
+ * */
+
+//gordon images postive
+const gordonPositiveImg1 = "images/goldStarGordon.jpeg";
+const gordonPositiveImg2 = "images/you-did-it-Gordon.jpg";
+//gordon images negative
+const gordonNegativeImg1 = "images/denseCabbageGordon.jpg";
+const gordonNegativeImg2 = "images/granGordon.jpeg";
+
+const renderFromAPI = document.getElementById("APIrender");
+renderFromAPI;
+const headerInstructions = document.getElementById("header-instructions");
+
 const nextButton = document.getElementById("nextQuestion");
-let num = 0;
 const trueButton = document.getElementById("trueButton");
 const falseButton = document.getElementById("falseButton");
+let userScore = document.getElementById("userScore");
+let changeImage = document.getElementById("gordonImg");
+let gordonImgContainer = document.getElementById("gordonImgContainer");
+let image = [gordonPositiveImg1, gordonNegativeImg1];
 let correctAnswer;
 let quizAPIData;
 let userInput;
-let feedbackResponse;
+let questionElement = document.getElementById("Question");
 let score = 0;
-let userScore = document.getElementById("userScore")
-let questionNumTracker = 1; 
-let image = ["./faislel well done.png", "./faislel wrong.png" ]
-let changeImage = document.getElementById("faisalImage")
+let num = 0;
+let questionNumTracker = 0;
 
-
-async function getQuestions() {
+async function getTenQuestions() {
   try {
-    const quizAPI = await fetch("https://opentdb.com/api.php?amount=10&type=boolean&difficulty=easy&encode=",
-      {
-        headers: 
-        {
-          "Accept": "application/json",
-        },
-      }
+    const quizAPI = await fetch(
+      "https://opentdb.com/api.php?amount=10&type=boolean&difficulty=easy"
     );
 
     quizAPIData = await quizAPI.json();
+    userScore.textContent = `Score: ${score} out of 10`;
     hideStart();
     nextQuestion();
+
     return quizAPIData;
-  } 
-  catch {
+  } catch {
     console.error("ERROR found");
   }
 }
 
-function hideStart(){
-  renderAPI.style.opacity = 0;
-  renderAPI.disabled = true;
+function hideStart() {
+  renderFromAPI.style.display = "none";
+  headerInstructions.style.display = "none";
 }
 
 function nextQuestion() {
-  if (questionNumTracker === quizAPIData.results.length){
-    endGame()
+  if (questionNumTracker === quizAPIData.results.length) {
+    endGame();
+  } else {
+    correctAnswer = quizAPIData.results[num].correct_answer;
+    let question = quizAPIData.results[num].question;
+    const decodedQuestion = decodeHTML(question);
+    questionElement.textContent = decodedQuestion;
+    questionNumTracker = num + 1;
+    hideGordon();
   }
-  else{
-  correctAnswer = quizAPIData.results[num].correct_answer;
-  let question = quizAPIData.results[num].question;
-  const decodedQuestion = decodeHTML(question);
-  const questionElement = document.getElementById("Question");
-  questionElement.textContent = decodedQuestion;
-  feedbackResponse.textContent = "";
-  questionNumTracker = num + 1
-  }
-
 }
 function decodeHTML(html) {
   let txt = document.createElement("textarea");
@@ -63,27 +72,33 @@ function decodeHTML(html) {
 
 function changeQuestion() {
   num = num + 1;
-  nextButton.disabled = true
-  nextButton.style.opacity = 0
-  changeImage.src = ""
+  nextButton.disabled = true;
+  nextButton.style.opacity = 0;
+  changeImage.src = "";
   nextQuestion();
 }
 
 function compareBool() {
-  feedbackResponse = document.getElementById("feedbackResponse");
   if (correctAnswer === userInput) {
-    feedbackResponse.textContent = "You get a point!!!";
-    score++
-    userScore.textContent = `Score = ${score} out of ${questionNumTracker}`
-    changeImage.src = image[0]
-
+    score++;
+    userScore.textContent = `Score: ${score} out of 10`;
+    changeImage.src = image[0];
+    showGordon();
   } else {
-    feedbackResponse.textContent = `Sorry the correct answer is ${correctAnswer}`;
-      userScore.textContent = `Score = ${score} out of ${questionNumTracker}`
-      changeImage.src = image[1]
+    userScore.textContent = `Score: ${score} out of 10`;
+    changeImage.src = image[1];
+    showGordon();
   }
-  nextButton.disabled = false
-  nextButton.style.opacity = 1 
+  nextButton.disabled = false;
+  nextButton.style.opacity = 1;
+}
+
+function hideGordon() {
+  gordonImgContainer.style.display = "none";
+}
+
+function showGordon() {
+  gordonImgContainer.style.display = "block";
 }
 
 function setTrue() {
@@ -96,14 +111,14 @@ function setFalse() {
   compareBool();
 }
 
-function endGame(){
-  let playerChoice = confirm(`congratulations you got ${score} correct`)
-  if (playerChoice === true){
+function endGame() {
+  let playerChoice = confirm(`congratulations you got ${score} correct`);
+  if (playerChoice === true) {
     location.reload();
   }
 }
 
-renderAPI.addEventListener("click", getQuestions);
+renderFromAPI.addEventListener("click", getTenQuestions);
 nextButton.addEventListener("click", changeQuestion);
 trueButton.addEventListener("click", setTrue);
 falseButton.addEventListener("click", setFalse);
